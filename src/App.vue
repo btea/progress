22a26b745727a2ca562f383f5dc3034730cbcdb3
btea <2356281422@qq.com>
2023-05-26 21:59:16 +0800

    feat: add new rect and circle

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,39 @@
 <script setup lang="ts">
-import { ref, onBeforeUnmount, onMounted } from 'vue'
+import { reactive, ref, onBeforeUnmount, onMounted } from 'vue'
 
-const path = ref()
-let length: number
+const one = ref()
+const two = ref()
+const three = ref()
+
+const infoLeng = reactive({
+  one: 0,
+  two: 0,
+  three: 0,
+  clock: 1
+})
 const render = (progress: number) => {
-  path.value.style.strokeDasharray = `${length * progress} ${length}`
+  one.value.style.strokeDasharray = `${infoLeng.one * progress} ${infoLeng.one}`
+  two.value.style.strokeDasharray = `${infoLeng.two * progress} ${infoLeng.two}`
+  three.value.style.strokeDasharray = `${infoLeng.three * progress} ${infoLeng.three}`
 }
 let progress = 0, timer: ReturnType<typeof window.requestAnimationFrame>
 const getProgress = () => {
-  progress += 0.001
+  progress += 0.001 * infoLeng.clock
   if (progress >= 1) {
-    progress = 0
-    window.cancelAnimationFrame(timer)
+    infoLeng.clock = -1
+  } 
+  if(progress <= 0) {
+    infoLeng.clock = 1
   }
+
   render(progress)
   timer = window.requestAnimationFrame(getProgress)
 }
 
 onMounted(() => {
-  length = path.value.getTotalLength()
+  infoLeng.one = one.value.getTotalLength()
+  infoLeng.two = two.value.getTotalLength()
+  infoLeng.three = three.value.getTotalLength()
   timer = window.requestAnimationFrame(getProgress)
 })
 onBeforeUnmount(() => {
@@ -28,7 +43,7 @@ onBeforeUnmount(() => {
 
 <template>
   <div class="view">
-    <svg viewBox="0 0 800 600">
+    <svg viewBox="0 0 600 600">
       <!-- custom gradient color -->
       <defs>
         <linearGradient id="gradient" x1="0" y1="0" x2="1" y2="1">
@@ -37,13 +52,15 @@ onBeforeUnmount(() => {
           <stop offset="100%" stop-color="#f00" />
         </linearGradient>
       </defs>
-      <path d="M 0,0 L 800,0 L 800,600 L 0,600 L 0,0" stroke-width="10" stroke="url(#gradient)" fill="none" ref="path"></path>
+      <path d="M 0,0 L 600,0 L 600,600 L 0,600 L 0,0 z" stroke-width="10" stroke="url(#gradient)" fill="none" ref="one"></path>
+      <path d="M 300,0 L 600,300 L 300,600 L 0,300 L 300,0" stroke-width="5" stroke="url(#gradient)" fill="none" ref="two"></path>
+      <circle cx="300" cy="300" r="200" stroke="url(#gradient)" fill="none" stroke-width="5" ref="three"></circle>
     </svg>
   </div>
 </template>
 <style lang="scss" scoped>
 .view {
-  width: 800px;
+  width: 600px;
   height: 600px;
   margin: 50px auto;
 }
