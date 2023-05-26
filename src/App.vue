2c0c8909fde8b249e770c2c67bde0873b3ec2f17
btea <2356281422@qq.com>
2023-05-26 21:25:36 +0800

    feat: add gradient

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ const render = (progress: number) => {
 }
 let progress = 0, timer: ReturnType<typeof window.requestAnimationFrame>
 const getProgress = () => {
-  progress += 0.002
+  progress += 0.001
   if (progress >= 1) {
     progress = 0
     window.cancelAnimationFrame(timer)
@@ -29,7 +29,15 @@ onBeforeUnmount(() => {
 <template>
   <div class="view">
     <svg viewBox="0 0 800 600">
-      <path d="M 0,0 L 800,0 L 800,600 L 0,600 L 0,0" stroke-width="10" stroke="#6cf" fill="none" ref="path"></path>
+      <!-- custom gradient color -->
+      <defs>
+        <linearGradient id="gradient" x1="0" y1="0" x2="1" y2="1">
+          <stop offset="0%" stop-color="#6cf" />
+          <stop offset="50%" stop-color="#09f" />
+          <stop offset="100%" stop-color="#f00" />
+        </linearGradient>
+      </defs>
+      <path d="M 0,0 L 800,0 L 800,600 L 0,600 L 0,0" stroke-width="10" stroke="url(#gradient)" fill="none" ref="path"></path>
     </svg>
   </div>
 </template>
@@ -37,6 +45,6 @@ onBeforeUnmount(() => {
 .view {
   width: 800px;
   height: 600px;
-  margin: 0 auto;
+  margin: 50px auto;
 }
 </style>
