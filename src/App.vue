b985ba7feac49b2a0b4adada273136954dcd9d0e
btea <2356281422@qq.com>
2023-05-27 23:06:27 +0800

    feat: add triangle

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,23 @@ import { reactive, ref, onBeforeUnmount, onMounted } from 'vue'
 const one = ref()
 const two = ref()
 const three = ref()
+const four = ref()
+const five = ref()
 
 const infoLeng = reactive({
   one: 0,
   two: 0,
   three: 0,
+  four: 0,
+  five: 0,
   clock: 1
 })
 const render = (progress: number) => {
   one.value.style.strokeDasharray = `${infoLeng.one * progress} ${infoLeng.one}`
   two.value.style.strokeDasharray = `${infoLeng.two * progress} ${infoLeng.two}`
   three.value.style.strokeDasharray = `${infoLeng.three * progress} ${infoLeng.three}`
+  four.value.style.strokeDasharray = `${infoLeng.four * progress} ${infoLeng.four}`
+  five.value.style.strokeDasharray = `${infoLeng.five * progress} ${infoLeng.five}`
 }
 let progress = 0, timer: ReturnType<typeof window.requestAnimationFrame>
 const getProgress = () => {
@@ -25,7 +31,6 @@ const getProgress = () => {
   if(progress <= 0) {
     infoLeng.clock = 1
   }
-
   render(progress)
   timer = window.requestAnimationFrame(getProgress)
 }
@@ -34,6 +39,8 @@ onMounted(() => {
   infoLeng.one = one.value.getTotalLength()
   infoLeng.two = two.value.getTotalLength()
   infoLeng.three = three.value.getTotalLength()
+  infoLeng.four = four.value.getTotalLength()
+  infoLeng.five = five.value.getTotalLength()
   timer = window.requestAnimationFrame(getProgress)
 })
 onBeforeUnmount(() => {
@@ -55,6 +62,10 @@ onBeforeUnmount(() => {
       <path d="M 0,0 L 600,0 L 600,600 L 0,600 L 0,0 z" stroke-width="10" stroke="url(#gradient)" fill="none" ref="one"></path>
       <path d="M 300,0 L 600,300 L 300,600 L 0,300 L 300,0" stroke-width="5" stroke="url(#gradient)" fill="none" ref="two"></path>
       <circle cx="300" cy="300" r="200" stroke="url(#gradient)" fill="none" stroke-width="5" ref="three"></circle>
+      <!-- 绘制三角形 -->
+      <path d="M 300,100 L 470,400 L 130,400 L 300,100" stroke-width="5" stroke="url(#gradient)" fill="none" ref="four"></path>
+      <!-- 反反向三角形 -->
+      <path d="M 300,500 L 130,200 L 470,200 L 300,500" stroke-width="5" stroke="url(#gradient)" fill="none" ref="five"></path>
     </svg>
   </div>
 </template>
